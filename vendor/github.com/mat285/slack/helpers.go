b5dd12195531f7eb2976a9922059cd9d0e172bcc
mat285 <mat285@cornell.edu>
2019-08-13 15:58:08 -0400

    fix

--- vendor/github.com/mat285/slack/helpers.go
+++ vendor/github.com/mat285/slack/helpers.go
@@ -57,8 +57,7 @@ func Notify(hook string, message *Message) error {
 // UnmarshalSlashCommandBody unmarshals the form encoded data into the struct
 func UnmarshalSlashCommandBody(body []byte) (*SlashCommandRequest, error) {
 	// TODO handle this better for unmarshalling why can't slack just use json
-	// strbody := strings.Replace(string(body), "\n&", "&", -1)
-	vals, err := url.ParseQuery(string(body))
+	vals, err := url.ParseQuery(strbody)
 	if err != nil {
 		return nil, exception.New(err)
 	}
